dfe397a1c6c10c810e8e8005ed4da1af82a6227e
Linh Huynh <cdev.linh@gmail.com>
2024-03-07 11:09:35 -0800

    Fixed bugs

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+// HTTP Logger
+app.use(morgan("combined"));
+
+// Template Engine
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+console.log("PATH:", path.join(__dirname, "resources/views"));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
